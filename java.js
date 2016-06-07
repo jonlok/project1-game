94a58080163e2182fec720a6ab72125400f67d35
jonlok <jonhklok@gmail.com>
2016-06-07 17:19:32 +0800

    added some basic javascript functions

--- java.js
+++ java.js
@@ -1,13 +1,67 @@
 // Pseudo code
-// 1) player click on different rooms to 'make a move'
 
-// 2) every click alternate between 1 and 2
+//-- 1) checkRoom function :
+// player click on different rooms to 'make a move'
 
-// 3) upon marking the room, do:
-//   a) room cannot be checked again
-//   b) change color of room and add variable "how many pineapples found in this room"
-// 4) reset button
 
+// ---2) every click alternate between 1   and 2
+
+//section--3)
+
+// upon marking the room, do:
+  // a) room cannot be checked again
+
+//   b) change color of room and
+
+// add variable "how many pineapples found in this room"
+
+ // c) check for this: each player allowed 3 moves so total maximum moves is 6
+
+// section--4) reset button function
+
+// reset function:
+   // a) setting room's innerHTML as empty
+   // b) clearing any classes in the room
 
 window.onload = start;
-var boxes = document.getElementsByClassName("room");
+var rooms = document.getElementsByClassName("room");
+var turnText = document.querySelector(".outputPrompt");
+var counter = 1;
+
+function start() {
+  checkRoomListener();
+  addResetListener();
+}
+
+function checkRoomListener () {
+  for (var i = rooms.length - 1; i >= 0; i--) {
+rooms[i].addEventListener("click",checkRoom);
+  }
+}
+
+function checkRoom (event) {
+ if (event.target.innerHTML.length === 0) {
+
+
+
+ }
+
+}
+
+function numOfPineapples () {
+  return parseInt(Math.random * 9);
+}
+
+//4) reset button
+function addResetListener(){
+  var resetButton = document.getElementById("reset");
+  resetButton.addEventListener("click", resetBoard);
+}
+
+function resetBoard(){
+  for (var i = rooms.length - 1; i >= 0 ; i--) {
+    rooms[i].innerHTML="";
+    rooms[i].setAttribute("class","clear");
+  }
+  turnText.innerHTML = "It is player one's turn";
+}
