e04fe4885fd8268b62b5595c88d60162fd3fed98
jonlok <jonhklok@gmail.com>
2016-06-13 13:38:25 +0800

    improved with the one click function

---
+++ testScript.js
@@ -0,0 +1,153 @@
+
+$(document).ready(function(){
+/*
+* sounds
+*/
+var mySound = new buzz.sound( "css/sound.wav",
+   { preload: true, loop: false });
+/*
+ *  Game variables
+ */
+var maxPineapples = 30;
+
+/*
+ *  Player object
+ */
+var Player = function(){
+  this.score = 0;
+};
+
+/*
+ *  Room object
+ */
+var Room = function(roomNum, element){
+  this.roomNumber = roomNum;
+  this.numberOfPineapples = Math.ceil( Math.random() * maxPineapples );
+  this.element = element;
+};
+
+
+/*
+ *  Game object
+ */
+var Game = function() {
+
+   var players = [];           // Array of players
+   this.activePlayer = null;    // The current active player
+   var rooms = [];             // Array of rooms
+   var turn = 0;               // The current  turn
+   var that = this;
+
+   /*
+    * 1) Adds the score to the player
+    */
+   function addScore(e){
+
+    var roomTarget = e.target;
+    var roomTargetNum = $(roomTarget).data('num');
+
+    var activeRoom = rooms.filter(function(room){
+        if(room.roomNumber == roomTargetNum){
+          return true;
+        }
+    });
+
+    activePlayer.score += activeRoom[0].numberOfPineapples;
+
+    $(roomTarget).fadeTo(3000, 0.15);
+    console.log('score added');
+    mySound.play();
+    console.log('sound');
+
+    }
+
+     /*
+     *   2) Determining who wins
+     */
+
+
+     // 3) function updateGUI(e){
+
+     //  $('.outputPrompt').text("It is player two's turn");
+
+     //  }
+
+     /*
+      *  4)  Take turn
+      *  This function runs by the end of each  turn
+      */
+
+     this.takeTurn = function(){
+        turn++;
+        activePlayer = players[turn%2];
+        console.log(activePlayer)
+        // when turn is 1,3,5 => player 1
+        // when turn is 2,4,6 => player 2
+        $('#score1').slideUp(300).slideDown(700).text(players[0].score);
+
+        $('#score2').slideUp(300).slideDown(700).text(players[1].score);
+
+      };
+
+
+     /*
+      *  5) Init
+      *  This function runs at the beginning of the game
+      */
+      this.init = function(){
+
+        // Clean up
+        players = [];           // Array of players
+        activePlayer = null;    // The current active player
+        rooms = [];             // Array of rooms
+        turn = 0;
+        $('#score1').text('0');
+        $('#score2').text('0');
+        $('.room').fadeTo(3000, 1);
+
+
+        // Create player 1
+        players.push( new Player() );
+
+        // Create player 2
+        players.push( new Player() );
+
+       // Init rooms
+      $('.room').each(function(index, element) {
+
+         var roomNumber = $(element).data('num');
+         rooms.push( new Room(roomNumber, element) );
+
+          $(element).one('click', function(e){
+            addScore(e);
+
+             that.takeTurn();
+          }   );
+
+        });
+
+        $('#reset').click(function(){
+          that.init();
+        });
+
+
+       /*
+       * 6) run the takeTurn function
+       */
+
+       this.takeTurn();
+
+
+       console.log('init');
+
+     };
+
+    this.init();
+ };
+
+
+// Create new Game
+var gameboard = new Game();
+
+
+});
